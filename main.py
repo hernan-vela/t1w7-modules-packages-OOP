f4f260e7a40b1675fbf6e057602b68942ddb336d
hernan-vela <hernan.velamar@gmail.com>
2024-06-27 21:56:36 +1000

    Uses of class and objects

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from text_processing import count_words, unique_words, count_characters, count_u
 test_string = "Hello World hello"
 
 # Using word_count module
-print("Word Count:", cw(test_string))
+print("Word Count:", count_words(test_string))
 print("Unique words:", unique_words(test_string))
 
 # Using char_count module
 print("Character Count:", count_characters(test_string))
 print("Unique Character Count:", count_unique_characters(test_string))
 
-# Using reverse module
-print("Reversed string:", reverse_string(test_string))
+# # Using reverse module
+# print("Reversed string:", reverse_string(test_string))
